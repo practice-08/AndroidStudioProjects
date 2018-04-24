90ceb8502f818ddd942cf6d99e98b3bb1b7d0674
practice-08 <31637987+practice-08@users.noreply.github.com>
2018-04-24 09:57:27 -0700

    Chypher

--- MadLibsPuppyLoveV01/app/src/main/java/com/example/palmdigital/madlibspuppylovev01/MainActivity.java
+++ MadLibsPuppyLoveV01/app/src/main/java/com/example/palmdigital/madlibspuppylovev01/MainActivity.java
@@ -28,17 +28,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         EditText et_bodyPart = findViewById(R.id.et_bodyPart);
         String bodyPartStr = et_bodyPart.getText().toString();
 
+        EditText et_nouns1 = findViewById(R.id.et_nouns1);
+        String nouns1Str = et_nouns1.getText().toString();
+
+        EditText et_verb1 = findViewById(R.id.et_verb1);
+        String verb1Str = et_verb1.getText().toString();
+
+        EditText et_adj1 = findViewById(R.id.et_adj1);
+        String adj1Str = et_adj1.getText().toString();
+
+        EditText et_adj2 = findViewById(R.id.et_adj2);
+        String adj2Str = et_adj2.getText().toString();
+
+        EditText et_verbs2 = findViewById(R.id.et_verbs2);
+        String verb2Str = et_adj2.getText().toString();
+
+        EditText et_noun2 = findViewById(R.id.et_noun2);
+        String noun2Str = et_adj2.getText().toString();
+
+        EditText et_noun3 = findViewById(R.id.et_noun3);
+        String noun3Str = et_adj2.getText().toString();
+
+
+
         String story = "Today I saw him again. When he looks at me with those ";
-        String story2 = "eyes, it makes my ";
+        story += colorStr;
+        story += " eyes, it makes my ";
+        story += bodyPartStr;
+        story += " go pitterpat, and I feel as if I have ";
+        story += nouns1Str;
+        story += " in my stomach. When he scrunches his nose, I want to ";
+        story += verb1Str;
+        story += " him softly. He is so ";
+        story += adj1Str;
+        story += " and ";
+        story += adj2Str;
+        story += " Tomorrow he will be mine. For now he ";
+        story += verb2Str;
+        story += " in the store ";
+        story += noun2Str;
+        story += " looking at me. ";
+        story += noun3Str;
+        story += " love in hard to resist ";
 
-        story = story + colorStr + story2 + bodyPartStr;
 
 
 
 
 
 
-        TextView output = findViewById(R.id.tvStory);
+
+                TextView output = findViewById(R.id.tvStory);
         output.setText(story);
 
 
